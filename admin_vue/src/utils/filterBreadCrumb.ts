9be78852a7ef24ebe5f80394ed53259a8d3ced51
张迪生 <2046353594@qq.com>
2024-11-11 17:05:01 +0800

    fix: 修复面包屑不显示问题

--- admin_vue/src/utils/filterBreadCrumb.ts
+++ admin_vue/src/utils/filterBreadCrumb.ts
@@ -1,24 +1,40 @@
 import { Breadcrumb, MenuList } from "@/store/types";
 export const filterBreadCrumb = (path: string, menuList: MenuList[]) => {
     let paths = path.split("/");
+
     //去空
     paths = paths.filter((item) => item);
+    console.log({ paths });
+
     const breadCrumbs: Breadcrumb[] = [];
-    paths.forEach((item) => {
+    paths.forEach((path) => {
+
+
+
         breadCrumbs.push({
-            name: getMenuTitle(item, menuList),
+            name: getMenuTitle(path, menuList),
         });
     });
+    console.log({ breadCrumbs });
+
     return breadCrumbs;
 };
 
 export const getMenuTitle = (path: string, menuList: MenuList[]): string => {
+
     for (let i in menuList) {
+
+
         if (menuList[i].path === path) {
+
             return menuList[i].meta.title!;
         }
+        //如果是目录,递归查找子元素下path
         if (menuList[i].menu_type === 1) {
-            return getMenuTitle(path, menuList[i].children) || ""
+            const title = getMenuTitle(path, menuList[i].children) || ""
+            if (title) {
+                return title;
+            }
         }
     }
     return ''

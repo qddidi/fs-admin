17c08a9bb4b876cb1e516d61bb94dedebd0158b2
张迪生 <2046353594@qq.com>
2024-12-19 17:55:00 +0800

    feat: 简化启动命令

--- admin_nest/ecosystem.config.js
+++ admin_nest/ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'Nest_APP',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      script: 'dist/main.js',
-      out_file: './log/file.log',
-      error_file: './log/file_error.log',
+      name: 'Nest_APP', //应用名称
+      log_date_format: 'YYYY-MM-DD HH:mm:ss', //日志格式
+      script: 'dist/main.js', //启动文件
+      out_file: './log/file.log', //日志文件
+      error_file: './log/file_error.log', //错误日志文件
       autorestart: true, //是否自动重启
-      instances: 2, //要启动实例的数量
+      instances: 2, //要启动实例的数量即负载数量
     },
   ],
 };
